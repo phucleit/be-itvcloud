0a334840fe751d54e6177dafc76b74014a49b50f
phucleit <phucleit95@gmail.com>
2022-05-12 09:07:38 +0700

    deploy

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use('/account', accountRouter);
 app.use('/login', loginRouter);
 
 // listen port
-app.listen(8000, () => {
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
     console.log('Server is running');
 })
