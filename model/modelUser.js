85e5334199bceb6572389fc19871fd939471bc27
phucleit <phucleit95@gmail.com>
2022-05-14 17:37:42 +0700

    required password

--- model/modelUser.js
+++ model/modelUser.js
@@ -11,7 +11,8 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     email: {
         type: String
